de4456931fbf0c82dacfad8ab607de1905f307bf
muink <hukk1996@gmail.com>
2022-12-18 01:06:38 +0800

    restart manually

--- htdocs/luci-static/resources/view/tinyfilemanager/config.js
+++ htdocs/luci-static/resources/view/tinyfilemanager/config.js
@@ -29,6 +29,14 @@ return view.extend({
 		s = m.section(form.TypedSection, 'main');
 		s.anonymous = true;
 
+		o = s.option(form.Button, '_restart', _('Restart') + ' tinyfilemanager');
+		o.inputtitle = _('Restart');
+		o.inputstyle = 'apply';
+		o.onclick = function() {
+			return fs.exec('/etc/init.d/tinyfilemanager', ['restart'])
+				.catch(function(e) { ui.addNotification(null, E('p', e.message), 'error') });
+		};
+
 		o = s.option(form.Flag, 'use_auth', _('Enable Authentication'));
 		o.rmempty = false;
 
