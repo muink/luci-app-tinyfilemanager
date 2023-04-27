8eb3a2008a2bae65f46b60b6ab6dc6d4385344d1
Anya Lin <hukk1996@gmail.com>
2023-04-27 10:46:40 +0800

    20230427

--- htdocs/luci-static/resources/view/tinyfilemanager/config.js
+++ htdocs/luci-static/resources/view/tinyfilemanager/config.js
@@ -14,14 +14,14 @@ return view.extend({
 	return Promise.all([
 		L.resolveDefault(fs.read('/var/tinyfilemanager/releaseslist'), null),
 		L.resolveDefault(fs.stat('/usr/libexec/tinyfilemanager-update'), {}),
-		L.resolveDefault(fs.stat('/usr/sbin/nginx'), {}),
+		L.resolveDefault(fs.stat('/etc/nginx/conf.d/php.locations'), {}),
 		uci.load('tinyfilemanager'),
 	]);
 	},
 
 	render: function(res) {
 		var releaseslist = res[0] ? res[0].trim().split("\n") : [],
-			has_nginx = res[2].path,
+			has_location = res[2].path,
 			pkgversion = '2.5.3';
 
 		var m, s, o;
@@ -38,8 +38,8 @@ return view.extend({
 			return fs.exec('/etc/init.d/tinyfilemanager', ['reload'])
 				.catch(function(e) { ui.addNotification(null, E('p', e.message), 'error') });
 		};
-		if (! has_nginx) 
-			o.description = _('To enable SSL support, you need to install <b>%s</b><br/>').format(['php8-nginx']);
+		if (! has_location) 
+			o.description = _('To enable SSL support, you may need to install <b>%s</b><br/>').format(['php-nginx']);
 
 		o = s.option(form.Flag, 'use_auth', _('Enable Authentication'));
 		o.rmempty = false;
